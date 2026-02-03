9ad946e692aa7d0a46f1c209da66a6a873ac8eac
Juan Rauda <juan.rauda4@gmail.com>
2026-02-02 18:40:47 -0600

    ajuste de archivo de firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+ firebase: {
+  apiKey: "AIzaSyC5ZIzCWoyzluzZXlijyvTrmtAXCA1bvSI",
+  authDomain: "basededatosps-72bbf.firebaseapp.com",
+  databaseURL: "https://basededatosps-72bbf-default-rtdb.firebaseio.com",
+  projectId: "basededatosps-72bbf",
+  storageBucket: "basededatosps-72bbf.firebasestorage.app",
+  messagingSenderId: "1036066888057",
+  appId: "1:1036066888057:web:d13920b25af0db3a81f7a8",
+  measurementId: "G-KWFMB5C7XW"
+    }
 };
