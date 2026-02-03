4d36f93b5523d4c4145bbc2478e2f4f6bc54c7a3
Juan Rauda <juan.rauda4@gmail.com>
2026-02-02 19:55:35 -0600

    barra de menu corregida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 import { AuthService } from 'src/service/auth.service';
-import { IonApp, IonButton } from "@ionic/angular/standalone";
+// import { IonApp, IonButton } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
-  standalone:false
+  standalone: false
 })
 export class AppComponent implements OnInit {
+
   isLoggedIn = false;
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private menuCtrl: MenuController
+  ) { }
 
   ngOnInit() {
     // Nos suscribimos al estado de login
@@ -21,24 +27,37 @@ export class AppComponent implements OnInit {
     });
   }
 
+  // 🔁 Método genérico: navegar + cerrar menú
+  navigateAndClose(route: string) {
+    this.menuCtrl.close('start');
+    this.router.navigate([route]);
+  }
+
   // Navegación pública
   goTohome() { this.router.navigate(['/home']); }
-  goToPublic1() { this.router.navigate(['/sustancias']); }
-  goToPublic2() { this.router.navigate(['/sensor']); }
-  goToPublic3() { this.router.navigate(['/public3']); }
-  goToPublic4 () { this.router.navigate(['/listar-documentos']); }
-
+  // goToPublic1() { this.router.navigate(['/sustancias']); }
+  // goToPublic2() { this.router.navigate(['/sensor']); }
+  // goToPublic3() { this.router.navigate(['/public3']); }
+  goToPublic4() { this.router.navigate(['/listar-documentos']); }
+  
   // Navegación privada
-  goToSensor() { this.router.navigate(['/sensor']); }
+  // goToSensor() { this.router.navigate(['/sensor']); }
   goToTempHumedad() { this.router.navigate(['/tempsensor']); }
-  goToSubirDoc(){ this.router.navigate(['/subir-documentos']); }
+  goToSubirDoc() { this.router.navigate(['/subir-documentos']); }
 
-  // Login / Logout
-  goToAuth() { this.router.navigate(['/auth']); }
+  // 🔑 Login
+  goToAuth() {
+    this.menuCtrl.close();
+    this.router.navigate(['/auth'], { replaceUrl: true });
+  }
 
+  // 🚪 Logout seguro
   logout() {
     this.authService.logout().then(() => {
-      this.router.navigate(['/home']);
+      this.menuCtrl.close();
+
+      // Redirige y elimina historial
+      this.router.navigateByUrl('/auth', { replaceUrl: true });
     });
   }
 }
